e91df225e54db8d728de6dd5440e00594a5a5f18
jesusqc <jesusquirocampos@gmail.com>
2023-12-11 01:44:40 +0100

    Flashlights Inventory

---
+++ OriginsSL/Modules/FlashlightsInventory/FlashlightsInventoryModule.cs
@@ -0,0 +1,33 @@
+using CursedMod.Events.Arguments.Player;
+using CursedMod.Events.Handlers;
+using PlayerRoles;
+using UnityEngine;
+
+namespace OriginsSL.Modules.FlashlightsInventory;
+
+public class FlashlightsInventoryModule : OriginsModule
+{
+    public override void OnLoaded()
+    {
+        CursedPlayerEventsHandler.Spawning += OnPlayerSpawning;
+    }
+
+    public override void OnUnloaded()
+    {
+        CursedPlayerEventsHandler.Spawning -= OnPlayerSpawning;
+    }
+    
+    private static void OnPlayerSpawning(PlayerSpawningEventArgs args)
+    {
+        if (args.RoleType != RoleTypeId.ClassD)
+            return;
+
+        if (Random.value > 0.5f) // 50% chance
+        {
+            args.Player.AddItem(ItemType.Coin);
+            return;
+        }
+        
+        args.Player.AddItem(ItemType.Flashlight);
+    }
+}
