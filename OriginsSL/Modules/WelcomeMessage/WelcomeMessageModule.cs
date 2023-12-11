9a9c27e9b9536b6564eed373aa33859c55cd1c97
jesusqc <jesusquirocampos@gmail.com>
2023-12-11 02:23:52 +0100

    new join message

--- OriginsSL/Modules/WelcomeMessage/WelcomeMessageModule.cs
+++ OriginsSL/Modules/WelcomeMessage/WelcomeMessageModule.cs
@@ -12,7 +12,6 @@ public class WelcomeMessageModule : OriginsModule
 
     private static void OnPlayerConnected(PlayerConnectedEventArgs args)
     {
-        args.Player.ShowBroadcast("Welcome to Origins SL! ");
-        // TODO: Enhance message
+        args.Player.ShowBroadcast($"<size=200%><b><color=#7cb0f9>{args.Player.DisplayNickname}</color></b></size>\n<b>Welcome to <color=#E2E0A6>o</color><color=#D8D4AC>r</color><color=#CEC8B2>i</color><color=#C4BCB8>g</color><color=#BAB0BE>i</color><color=#B0A4C4>n</color><color=#A698CA>s</color> <color=#9280D6>s</color><color=#8874DC>l \ud83d\udcab</color>");
     }
 }
