1b703e1400961bf063a0ee1f49dfcd511000b318
jesusqc <jesusquirocampos@gmail.com>
2023-12-11 11:10:35 +0100

    chaos replace facility guards

---
+++ OriginsSL/Modules/ChaosReplaceFacilityGuards/ChaosReplaceFacilityGuardsModule.cs
@@ -0,0 +1,33 @@
+using CursedMod.Events.Arguments.Player;
+using CursedMod.Events.Handlers;
+using PlayerRoles;
+using UnityEngine;
+
+namespace OriginsSL.Modules.ChaosReplaceFacilityGuards;
+
+public class ChaosReplaceFacilityGuardsModule : OriginsModule
+{
+    private static bool _chaosSpawn;
+    
+    public override void OnLoaded()
+    {
+        CursedRoundEventsHandler.WaitingForPlayers += OnWaitingForPlayers;
+        CursedPlayerEventsHandler.ChangingRole += OnChangingRole;
+    }
+    
+    private static void OnWaitingForPlayers()
+    {
+        _chaosSpawn = Random.value < 0.2f; // 20% chance
+    }
+    
+    private static void OnChangingRole(PlayerChangingRoleEventArgs args)
+    {
+        if (!_chaosSpawn || args.ChangeReason != RoleChangeReason.RoundStart)
+            return;
+        
+        if (args.NewRole != RoleTypeId.FacilityGuard)
+            return;
+        
+        args.NewRole = RoleTypeId.ChaosConscript;
+    }
+}
