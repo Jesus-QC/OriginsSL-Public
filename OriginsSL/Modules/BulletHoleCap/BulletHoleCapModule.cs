2783b359d23af9c2c0fe28ed025eb760c5b8c42f
jesusqc <jesusquirocampos@gmail.com>
2023-12-12 23:55:44 +0100

    BulletHoleCap

---
+++ OriginsSL/Modules/BulletHoleCap/BulletHoleCapModule.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using CursedMod.Events.Arguments.Player;
+using CursedMod.Events.Handlers;
+using CursedMod.Features.Wrappers.Player;
+
+namespace OriginsSL.Modules.BulletHoleCap;
+
+public class BulletHoleCapModule : OriginsModule
+{
+    public override void OnLoaded()
+    {
+        CursedPlayerEventsHandler.PlacingBulletHole += OnPlacingBulletHole;
+        CursedRoundEventsHandler.RestartingRound += OnRestartingRound;
+    }
+    
+    private static readonly Dictionary<CursedPlayer, byte> BulletHoleCounter = new();
+    
+    private static void OnRestartingRound()
+    {
+        BulletHoleCounter.Clear();
+    }
+    
+    private static void OnPlacingBulletHole(PlayerPlacingBulletHoleEventArgs args)
+    {
+        if (BulletHoleCounter.TryGetValue(args.Player, out byte count))
+        {
+            if (count >= 50)
+            {
+                args.IsAllowed = false;
+                return;
+            }
+            
+            BulletHoleCounter[args.Player]++;
+            return;
+        }
+        
+        BulletHoleCounter.Add(args.Player, 1);
+    }
+}
