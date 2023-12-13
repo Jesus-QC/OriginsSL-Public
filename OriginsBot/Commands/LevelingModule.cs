9bd7dd532fb63634bfdf24ad166231878546943f
jesusqc <jesusquirocampos@gmail.com>
2023-12-13 13:55:31 +0100

    Clipping text

--- OriginsBot/Commands/LevelingModule.cs
+++ OriginsBot/Commands/LevelingModule.cs
@@ -9,7 +9,7 @@ public class LevelingModule : InteractionModuleBase<SocketInteractionContext>
     public async Task LevelingCommand([Summary(description:"Member which you'd like to view progress")]IUser? member = null)
     {
         member ??= Context.User;
-        LevelingImageBuilder levelingImageBuilder = new (member.Username, Random.Shared.Next(0, 9999), Random.Shared.Next(0, 9999), Random.Shared.Next(0, 9999), Random.Shared.Next(0, 9999));
+        LevelingImageBuilder levelingImageBuilder = new (member.Username, Random.Shared.Next(0, 9999), Random.Shared.Next(0, 5000), 5000, Random.Shared.Next(0, 9999));
         await RespondWithFileAsync(await levelingImageBuilder.BuildAsync(), "level.png");
     }
 }
--- OriginsBot/LevelingImageBuilder.cs
+++ OriginsBot/LevelingImageBuilder.cs
@@ -1,9 +1,11 @@
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using Path = System.IO.Path;
 
 namespace OriginsBot;
 
@@ -25,6 +27,12 @@ public class LevelingImageBuilder
         {
             Origin = position,
             HorizontalAlignment = HorizontalAlignment.Center,
+        };
+        
+        public static RichTextOptions GetTextOptions(Font font, PointF position) => new (font)
+        {
+            Origin = position,
+            HorizontalAlignment = HorizontalAlignment.Left,
         };
         
         static FontRetriever()
@@ -55,9 +63,15 @@ public class LevelingImageBuilder
 
     public async Task<Stream> BuildAsync()
     {
-        Image image = await Image.LoadAsync(Path.Combine(FontRetriever.DataDirectory, "Blank_75.png"));
+        Image image = await Image.LoadAsync(Path.Combine(FontRetriever.DataDirectory, $"Blank_{GetValue(Xp, XpToNextLevel)}.png"));
         
-        image.Mutate(x => x.DrawText(Username, FontRetriever.UsernameFont, new SolidBrush(FontRetriever.MainColor), new SolidPen(Color.Black, 4), new PointF(36, 26)));
+        image.Mutate(x => x.Clip(new Polygon(new []
+        {
+            new PointF(22, 14),
+            new PointF(22,78),
+            new PointF(554, 78),
+            new PointF(554, 14),
+        }), y => y.DrawText(FontRetriever.GetTextOptions(FontRetriever.UsernameFont, new PointF(36, 26)), Username, new SolidBrush(FontRetriever.MainColor), new SolidPen(Color.Black, 4))));
         image.Mutate(x => x.DrawText(FontRetriever.GetCenteredOptions(FontRetriever.LevelFont, new PointF(175, 89)), Level.ToString().PadLeft(4, '0'), new SolidBrush(FontRetriever.SecondaryColor), new SolidPen(Color.Black, 4)));
         image.Mutate(x => x.DrawText(FontRetriever.GetCenteredOptions(FontRetriever.ExpFont, new PointF(286, 161)),Xp.ToString().PadLeft(4, '0') + "/" + XpToNextLevel.ToString().PadLeft(4, '0'), new SolidBrush(FontRetriever.SecondaryColor), new SolidPen(Color.Black, 2)));
         image.Mutate(x => x.DrawText(FontRetriever.GetCenteredOptions(FontRetriever.RankFont, new PointF(702, 90)),"#" + Rank, new SolidBrush(FontRetriever.SecondaryColor), new SolidPen(Color.Black, 4)));
@@ -66,4 +80,22 @@ public class LevelingImageBuilder
         await image.SaveAsync(stream, new PngEncoder());
         return stream;
     }
+    
+    private static string GetValue(int xp, int xpToNextLevel)
+    {
+        int value = (int) Math.Round((double) xp / xpToNextLevel * 100);
+
+        foreach (int v in Values)
+        {
+            if (value < v) 
+                return v.ToString();
+        }
+
+        return string.Empty;
+    }
+
+    private static readonly HashSet<int> Values = new()
+    {
+        0,10,25,45,60,75,90,95
+    };
 }
