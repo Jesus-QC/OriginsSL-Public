c33f58ee2ff77cf694b63515ef164032270828a8
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2023-12-14 09:39:30 +0100

    Comments

--- OriginsSL/Modules/LevelingSystem/LevelingSystemModule.cs
+++ OriginsSL/Modules/LevelingSystem/LevelingSystemModule.cs
@@ -1,5 +1,7 @@
 namespace OriginsSL.Modules.LevelingSystem;
 
+// TODO: mariadb integrated db in dedi
+// PD: Nexus, do not use sqlite
 public class LevelingSystemModule : OriginsModule
 {
     private static readonly LevelingSystemEventsHandler EventsHandler = new ();
