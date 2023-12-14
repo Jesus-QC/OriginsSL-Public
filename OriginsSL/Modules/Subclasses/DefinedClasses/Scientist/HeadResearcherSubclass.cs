e70be06ebd816c479c12e2d23d2912b0e59c8cea
jesusqc <jesusquirocampos@gmail.com>
2023-12-14 02:51:07 +0100

    Fixes

--- OriginsSL/Modules/Subclasses/DefinedClasses/Scientist/HeadResearcherSubclass.cs
+++ OriginsSL/Modules/Subclasses/DefinedClasses/Scientist/HeadResearcherSubclass.cs
@@ -5,9 +5,9 @@ namespace OriginsSL.Modules.Subclasses.DefinedClasses.Scientist;
 public class HeadResearcherSubclass : SubclassBase
 {
     public override string CodeName => "headresearcher";
-    public override string Name => "<color=##FFFF7C>H<lowercase>ead</lowercase> R<lowercase>esearcher</lowercase></color>";
+    public override string Name => "<color=#FFFF7C>H<lowercase>ead</lowercase> R<lowercase>esearcher</lowercase></color>";
     public override string Description => "well-known scientist, higher level clearance";
-    public override float SpawnChance => 1000f;
+    public override float SpawnChance => 0.25f;
     public override List<ItemType> Inventory { get; } = [ItemType.KeycardResearchCoordinator, ItemType.Medkit, ItemType.Flashlight];
 
 }
