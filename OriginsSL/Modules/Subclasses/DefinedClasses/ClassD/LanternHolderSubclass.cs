8e6d858a4873666d8e586065ef656e7d664d2bb4
jesusqc <jesusquirocampos@gmail.com>
2023-12-19 04:49:39 +0100

    gg

--- OriginsSL/Modules/Subclasses/DefinedClasses/ClassD/LanternHolderSubclass.cs
+++ OriginsSL/Modules/Subclasses/DefinedClasses/ClassD/LanternHolderSubclass.cs
@@ -8,8 +8,6 @@ public class LanternHolderSubclass : SubclassBase
     public override string Name => "<color=#e3bb76>L<lowercase>antern</lowercase> H<lowercase>older</lowercase></color>";
     public override string Description => "you hold peace in your hands";
     public override float SpawnChance => 0.5f;
-    
     public override bool KeepAfterEscaping => true;
-
     public override List<ItemType> AdditiveInventory { get; } = [ItemType.Lantern];
 }
