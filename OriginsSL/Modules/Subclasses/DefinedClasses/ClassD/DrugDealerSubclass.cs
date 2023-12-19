10c227224be82ff9f9ad2fd87a28588ea77655c9
jesusqc <jesusquirocampos@gmail.com>
2023-12-19 07:04:15 +0100

    .4f

--- OriginsSL/Modules/Subclasses/DefinedClasses/ClassD/DrugDealerSubclass.cs
+++ OriginsSL/Modules/Subclasses/DefinedClasses/ClassD/DrugDealerSubclass.cs
@@ -8,7 +8,7 @@ public class DrugDealerSubclass : PitchChangerSubclass
     public override string CodeName => "drugdealer";
     public override string Name => "<color=#059033>D<lowercase>rug</lowercase> D<lowercase>ealer</lowercase></color>";
     public override string Description => "you may keep some drugs in your pockets";
-    public override float SpawnChance => 0.5f;
+    public override float SpawnChance => 0.4f;
     public override bool KeepAfterEscaping => true;
     public override List<ItemType> AdditiveInventory { get; } = [ItemType.Painkillers, ItemType.Painkillers, ItemType.Adrenaline];
     
